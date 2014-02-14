4e021da7e6d6f253c6770173d0794b2c983a2e7e
Zoltán Adamek <zadamek@newpush.com>
2014-02-14 13:12:15 +0100

    Fixes JS and CSS loading.

--- fullpage-js-wp/fullpage-js-wp.php
+++ fullpage-js-wp/fullpage-js-wp.php
@@ -38,9 +38,9 @@ function fullpage_register_ctype() {
 function fullpage_enqueue_scripts() {
   if (is_single() && get_query_var('fullpage')) {
     wp_enqueue_script('jquery');
-    wp_enqueue_script('fullpage', get_template_directory_uri() . '/js/jquery.fullPage.min.js');
-    wp_enqueue_script('fullpage-init', get_template_directory_uri() . '/js/fullPage-init.js');
-    wp_enqueue_script('fullpage-css', get_template_directory_uri() . '/js/jquery.fullPage.css');
+    wp_enqueue_script('fullpage', plugins_url('/js/jquery.fullPage.min.js', __FILE__));
+    wp_enqueue_script('fullpage-init', plugins_url('/js/fullPage-init.js', __FILE__));
+    wp_enqueue_script('fullpage-css', plugins_url('/js/jquery.fullPage.css', __FILE__));
   }
 }
 
