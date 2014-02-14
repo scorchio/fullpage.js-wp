38e76ee471d3fa9eb87ebdd2c6ba30b2bc9d5ab9
Zoltán Adamek <zadamek@newpush.com>
2014-02-14 11:57:53 +0100

    Added basic header.

---
+++ fullpage-js-wp/fullpage-js-wp.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Fullpage.js
+ *
+ * Plugin Name:       Fullpage.js
+ * Description:       WP immplementation for Alvaro Trigo's Fullpage.js library.
+ * Version:           1.0.0
+ * GitHub Plugin URI: https://github.com/scorchio/fullpage.js-wp
+ * GitHub Branch:     master
+ */
