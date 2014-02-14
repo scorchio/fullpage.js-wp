79bd60dfad9c254df6b5392d4ea8cc99e6ac4783
Zoltán Adamek <zadamek@newpush.com>
2014-02-14 12:38:19 +0100

    Adds content type.

--- fullpage-js-wp/fullpage-js-wp.php
+++ fullpage-js-wp/fullpage-js-wp.php
@@ -6,3 +6,32 @@
  * GitHub Plugin URI: https://github.com/scorchio/fullpage.js-wp
  * GitHub Branch:     master
  */
+
+add_action('init', 'fullpage_register_ctype');
+
+function fullpage_register_ctype() {
+  $labels = array(
+    'menu_name' => _x('Fullpage', 'fullpage'),
+  );
+
+  $args = array(
+    'labels' => $labels,
+    'hierarchical' => TRUE,
+    'description' => 'Fullpage',
+    'supports' => array('title', 'editor'),
+    'public' => TRUE,
+    'show_ui' => TRUE,
+    'show_in_menu' => TRUE,
+    'show_in_nav_menus' => TRUE,
+    'publicly_queryable' => TRUE,
+    'exclude_from_search' => FALSE,
+    'has_archive' => TRUE,
+    'query_var' => TRUE,
+    'can_export' => TRUE,
+    'rewrite' => TRUE,
+    'capability_type' => 'post'
+  );
+
+  register_post_type('fullpage', $args);
+}
+
