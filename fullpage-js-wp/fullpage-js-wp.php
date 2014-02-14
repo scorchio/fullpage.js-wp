8bc8bdb240b27f0ebe2f42c294f960a0b6898391
Zoltán Adamek <zadamek@newpush.com>
2014-02-14 12:16:22 +0100

    Modifies plugin header for testing.

--- fullpage-js-wp/fullpage-js-wp.php
+++ fullpage-js-wp/fullpage-js-wp.php
@@ -1,9 +1,5 @@
 <?php
 /**
- * Fullpage.js
- *
- * WP immplementation for Alvaro Trigo's Fullpage.js library.
- *
  * Plugin Name:       Fullpage.js
  * Description:       WP immplementation for Alvaro Trigo's Fullpage.js library.
  * Version:           1.0.0
